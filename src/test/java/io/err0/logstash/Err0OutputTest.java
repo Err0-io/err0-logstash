9c1cafac50f61787e4c65d0518973c5dfa89d4a2
Ben Gould <ben@amb1ent.org>
2022-07-20 16:18:29 +0100

    testing with kubernetes error codes, from our open-source-bundle

--- src/test/java/io/err0/logstash/Err0OutputTest.java
+++ src/test/java/io/err0/logstash/Err0OutputTest.java
@@ -15,19 +15,21 @@ public class Err0OutputTest {
     @Test
     public void testJavaOutputExample() {
         Map<String, Object> configValues = new HashMap<>();
-        configValues.put(Err0Output.URL.name(), "https://open-source-dev.err0.io:8443/");
-        configValues.put(Err0Output.TOKEN.name(), "2.pHkDUXHbM-S-OkuePiq8NVdDN6ANtwwVPt1nzg5RkZw_oIcvWPyll61ulTtk5cdq-cqVtA3IKwjK7cAAYhDs0IosqkEoOjgMU7P4c5QdjwnusoPZqeND0w.SQ8R3sHTFQ5RJkrM-9bwTLRBkK1zNmeXhbj7W9KEcps");
+        configValues.put(Err0Output.URL.name(), "https://open-source.dev.err0.io:8443/");
+        configValues.put(Err0Output.TOKEN.name(), "2.MFRF-aOr9f8BS9oMHGatWXfIYGY_m5IPWpMQPccLxXZqSSGGx8JJgsJOfYwLtelzgZu63kZIpZGpnQW5sw_wAQbGQvHQxAHz421yrEZuk0zywZm_Gt4kVw.SVFJddGR3KTR_nAA_bYivKrIfh0GbvsXLxY0x2qOlug");
 
         Configuration config = new ConfigurationImpl(configValues);
         Err0Output output = new Err0Output("test-id", config, null);
 
         String sourceField = "message";
-        int eventCount = 5;
+        int eventCount = 40;
         Collection<Event> events = new ArrayList<>();
         for (int k = 0; k < eventCount; k++) {
-            Event e = new org.logstash.Event();
-            e.setField(sourceField, "[EG-" + k + "] example log line");
-            events.add(e);
+            for (int i = 0, l = (int)(Math.random() * 10.0); i < l; i++) {
+                Event e = new org.logstash.Event();
+                e.setField(sourceField, "[KUB-" + k + "] example log line");
+                events.add(e);
+            }
         }
 
         output.output(events);
