4c8e84acccce5d3236283519aaea3d53332e0980
Ben Gould <ben@amb1ent.org>
2022-12-13 12:28:46 +0000

    to fix unit test for log appender data

--- src/test/java/io/err0/logstash/Err0OutputTest.java
+++ src/test/java/io/err0/logstash/Err0OutputTest.java
@@ -17,15 +17,15 @@ public class Err0OutputTest {
         Map<String, Object> configValues = new HashMap<>();
         // the token/url is in the starter database, in the junit tests (with UAT data).
         configValues.put(Err0Output.URL.name(), "https://open-source.dev.err0.io:8443/");
-        configValues.put(Err0Output.TOKEN.name(), "2.orm1bva6H8SDiB5BC1QvrFxgrYigD6bzDujAQgXxtrrvcl7hwGTHye8LD-_LmGq-ksZ8TbnXOmPVdbZr-AM8s4zgeLXfmQPCMJCtYs2CsqihrdtI9V8JGg.BLSWe3LlwUtg7SVnPkZInQt1TyIg9vuR3LA5l0JQ0tM");
+        configValues.put(Err0Output.TOKEN.name(), "2.rFxu4p0eWKD1JnK8OI8Qj7R2W_cLDhdhn9WgMD3IcFYpgXhxLZbruQHCiF797ganf4tHFFsPxE_rjJyyfYh1W942WFFvGk0iB9VK1HFbhJXlFYOkNYKAuA.4ECkWt0gggCdvNwZsmEYrTEtTmPAWqiG-lo6ox6c3S0");
 
         Configuration config = new ConfigurationImpl(configValues);
         Err0Output output = new Err0Output("test-id", config, null);
 
         String sourceField = "message";
-        int eventCount = 40;
+        int eventCount = 100;
         Collection<Event> events = new ArrayList<>();
-        for (int k = 0; k < eventCount; k++) {
+        for (int k = 1; k <= eventCount; k++) {
             for (int i = 0, l = (int)(Math.random() * 10.0); i < l; i++) {
                 Event e = new org.logstash.Event();
                 e.setField(sourceField, "[KUB-" + k + "] example log line");
